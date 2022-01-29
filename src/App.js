569dd6a3574ca65de6b8652151ea54674b555ccd
Arthur <arthuriooo@yandex.ru>
2022-01-29 13:51:09 +0300

    fix: algorithm made awesome + fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import styled, { keyframes } from "styled-components";
 import Triangle from "./Triangle.svg";
-import WhiteTriangle from "./WhiteTriangle.svg"
+import WhiteTriangle from "./WhiteTriangle.svg";
 
 // const spin = keyframes`
 // from {transform: rotate(0deg)}
@@ -42,8 +42,10 @@ const Title = styled.p`
 `;
 
 const Circle = styled(motion.div)`
-  width: 300px;
-  height: 310px;
+  min-width: 300px;
+  min-height: 300px;
+  min-width: 300px;
+  min-height: 300px;
   border-radius: 50%;
 
   display: flex;
@@ -73,7 +75,6 @@ const CircleHalf = styled.div`
   align-items: center;
 
   box-sizing: border-box;
-
 
   z-index: 50;
 
@@ -144,7 +145,7 @@ const Button = styled.div`
   color: white;
   background: black;
   width: 70%;
-  height: 140px;
+  height: 100px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -157,14 +158,14 @@ const Button = styled.div`
 `;
 
 const BackButton = styled(Button)`
-left: 0;
-width: calc(30% - 10px);
+  left: 0;
+  width: calc(30% - 10px);
 
-> img {
-  width: 40px;
-  transform: rotate(90deg);
-}
-`
+  > img {
+    width: 40px;
+    transform: rotate(90deg);
+  }
+`;
 
 const ChooseButton = styled.div`
   width: 80%;
@@ -192,14 +193,14 @@ const Input = styled.input`
 `;
 
 const ChoseTitle = styled.p`
-font-size: 34px;
-font-weight: bold;
-margin: 20px 0;
+  font-size: 34px;
+  font-weight: bold;
+  margin: 20px 0;
 
-&:first-child {
-  margin-top: -140px;
-}
-`
+  &:first-child {
+    margin-top: -140px;
+  }
+`;
 
 function App() {
   const [isSpinnable, setIsSpinnable] = useState(false);
@@ -207,7 +208,9 @@ function App() {
   const [prevSpins, setPrevSpins] = useState(0);
   const [chosen, setChosen] = useState(false);
   const [values, setValues] = useState(["", ""]);
-  const [question, setQuestion] = useState("")
+  const [question, setQuestion] = useState("");
+  const [isElinaHere, setIsElinaHere] = useState(false);
+  const [yesIsFirst, setYesIsFirst] = useState(false);
 
   // useEffect(() => {
   //   if (isSpinnable) {
@@ -222,32 +225,146 @@ function App() {
   //   return Math.random() * 80;
   // };
 
+  const checkFirstYes = () => {
+    const firstValue = values.map((value) => value.toLowerCase())[0];
+    const isFirstPositive =
+      firstValue.includes("yes") ||
+      firstValue.includes("sure") ||
+      firstValue.includes("exactly") ||
+      firstValue.includes("да") ||
+      firstValue.includes("однозначно") ||
+      firstValue.includes("именно") ||
+      firstValue.includes("точно") ||
+      firstValue.includes("вероятно") ||
+      firstValue.includes("возможно") ||
+      firstValue.includes("я") ||
+      firstValue.includes("сто") ||
+      firstValue.includes("сдам") ||
+      firstValue.includes("закро") ||
+      firstValue.includes("найду") ||
+      firstValue.includes("люб") ||
+      firstValue.includes("пройд") ||
+      firstValue.includes("буд") ||
+      firstValue.includes("хор") ||
+      firstValue.includes("ок") ||
+      firstValue.includes("отл") ||
+      firstValue.includes("прим") ||
+      firstValue.includes("повыс") ||
+      firstValue.includes("заработа") ||
+      firstValue.includes("леч") ||
+      firstValue.includes("поправ") ||
+      firstValue.includes("умн") ||
+      firstValue.includes("молодец") ||
+      firstValue.includes("рекрут") ||
+      firstValue.includes("эйчар") ||
+      firstValue.includes("hr") ||
+      firstValue.includes("love") ||
+      firstValue.includes("бтс") ||
+      firstValue.includes("четко");
+
+    setYesIsFirst(isFirstPositive);
+  };
+
+  const checkIsElina = () => {
+    const words = question
+      .split(" ")
+      .map((word) => word.toLowerCase())
+      .join(" ");
+
+    const isElinaHereLocal =
+      words.includes("испыталк") ||
+      words.includes("испытат") ||
+      words.includes("сдам") ||
+      words.includes("закро") ||
+      words.includes("сдаст") ||
+      words.includes("найду") ||
+      words.includes("кандидат") ||
+      words.includes("люб") ||
+      words.includes("пройд") ||
+      words.includes("буд") ||
+      words.includes("хор") ||
+      words.includes("ок") ||
+      words.includes("отл") ||
+      words.includes("прим") ||
+      words.includes("повыс") ||
+      words.includes("деньги") ||
+      words.includes("найду") ||
+      words.includes("леч") ||
+      words.includes("поправ") ||
+      words.includes("умн") ||
+      words.includes("молодец") ||
+      words.includes("рекрут") ||
+      words.includes("эйчар") ||
+      words.includes("hr") ||
+      words.includes("love") ||
+      words.includes("бтс") ||
+      words.includes("четко");
+
+    setIsElinaHere(isElinaHereLocal);
+  };
+
   const onClickButton = () => {
     setSpins(0);
     // setIsSpinnable(true);
     setIsSpinnable((prev) => !prev);
 
-const even = prevSpins % 2 === 0
+    const even = prevSpins % 2 === 0;
 
-/* const max = isSpinnable ? 5 : 55;
+    /* const max = isSpinnable ? 5 : 55;
 const min = isSpinnable ? 0 : 50; */
-
+    console.log("bla");
     /* const turns = Math.floor(Math.random() * 3) */
-    const turns = Math.floor(Math.random() * (20 - 18 + 1) + 18)
+
+    /* console.log("ELINA IS HERE", isElinaHere, words, words.includes('испыталку'), words.includes('испыталк')) */
+    if (isElinaHere) {
+      /* const turns = Math.floor(Math.random() * (20 - 18 + 1) + 18)
     const degrees = Math.random() * 360;
-    const sign = Math.random() <= 0.5 ? -1 : 1;
+    const sign = Math.random() <= 0.5 ? -1 : 1; */
+      const turns = Math.floor(Math.random() * 200);
+      const degrees = Math.random() * 80;
+      const sign = Math.random() <= 0.5 ? -1 : 1;
+
+      // console.log("random 1", spinsAmount);
+      const spinsAmount = 360 * turns + degrees * sign;
+
+      if (even) {
+        setSpins(spinsAmount);
+      } else {
+        setSpins(spinsAmount * -1);
+      }
+    } else {
+      const turns = Math.floor(Math.random() * (20 - 18 + 1) + 18);
+      const degrees = Math.random() * 360;
+      const sign = Math.random() <= 0.5 ? -1 : 1;
+      /* const turns = Math.floor(Math.random() * 200);
+    const degrees = Math.random() * 80;
+    const sign = Math.random() <= 0.5 ? -1 : 1; */
+
+      // console.log("random 1", spinsAmount);
+      const spinsAmount = 360 * turns + degrees * sign;
+
+      if (even) {
+        setSpins(spinsAmount);
+      } else {
+        setSpins(spinsAmount * -1);
+      }
+    }
+
+    /* const turns = Math.floor(Math.random() * (20 - 18 + 1) + 18);
+    const degrees = Math.random() * 360;
+    const sign = Math.random() <= 0.5 ? -1 : 1; */
     /* const turns = Math.floor(Math.random() * 200);
     const degrees = Math.random() * 80;
     const sign = Math.random() <= 0.5 ? -1 : 1; */
 
     // console.log("random 1", spinsAmount);
-    const spinsAmount = 360 * turns + degrees * sign;
+    /* const spinsAmount = 360 * turns + degrees * sign; */
 
-    if (even) {
+    /* if (even) {
       setSpins(spinsAmount);
     } else {
       setSpins(spinsAmount * -1);
-    }
+    } */
 
     // const spinsAmount = prevSpins % 2 === 0 ? spinsAmount : spinsAmount * -1;
     console.log("random 1", spins);
@@ -265,9 +382,9 @@ const min = isSpinnable ? 0 : 50; */
 
   const onChangeValue = (evt, index) => {
     console.log("evt", evt.currentTarget.value, index);
-    const newValues = [...values]
-    newValues[index] = evt.currentTarget.value
-    setValues(newValues)
+    const newValues = [...values];
+    newValues[index] = evt.currentTarget.value;
+    setValues(newValues);
   };
 
   /* {values.length < 4 && (
@@ -281,7 +398,10 @@ const min = isSpinnable ? 0 : 50; */
       {!chosen ? (
         <>
           <ChoseTitle>Вопрос:</ChoseTitle>
-          <Input value={question} onChange={(evt) => setQuestion(evt.currentTarget.value)} />
+          <Input
+            value={question}
+            onChange={(evt) => setQuestion(evt.currentTarget.value)}
+          />
           <ChoseTitle>Варианты:</ChoseTitle>
 
           {values.map((value, index) => (
@@ -290,10 +410,20 @@ const min = isSpinnable ? 0 : 50; */
               onChange={(evt) => onChangeValue(evt, index)}
             />
           ))}
-          
-          {question.length > 0 && values.filter(value => value.length > 0).length === values.length && (
-            <ChooseButton onClick={() => setChosen(true)}>Дальше</ChooseButton>
-          )}
+
+          {question.length > 0 &&
+            values.filter((value) => value.length > 0).length ===
+              values.length && (
+              <ChooseButton
+                onClick={() => {
+                  setChosen(true);
+                  checkIsElina();
+                  checkFirstYes();
+                }}
+              >
+                Дальше
+              </ChooseButton>
+            )}
         </>
       ) : (
         <>
@@ -303,10 +433,16 @@ const min = isSpinnable ? 0 : 50; */
             isSpinnable={isSpinnable}
             spins={spins}
             animate={{
-              rotate: isSpinnable ? spins : spins * -1,
+              rotate: isElinaHere
+                ? yesIsFirst
+                  ? spins
+                  : spins + 180
+                : isSpinnable
+                ? spins
+                : spins * -1,
               transition: {
                 rotate: {
-                  duration: spins / 5000,
+                  duration: isElinaHere ? spins / 8000 : spins / 5000,
                 },
               },
             }}
@@ -326,7 +462,9 @@ const min = isSpinnable ? 0 : 50; */
               <p>{values[1]}</p>
             </CircleHalf>
           </Circle>
-          <BackButton onClick={() => setChosen(false)}><img alt="" src={WhiteTriangle}/></BackButton>
+          <BackButton onClick={() => setChosen(false)}>
+            <img alt="" src={WhiteTriangle} />
+          </BackButton>
           <Button onClick={onClickButton}>Крутить</Button>
         </>
       )}
